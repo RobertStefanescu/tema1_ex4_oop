e589f02fbc7593d3774f6e99ac939c5901287c7c
RobertStefanescu <62601851+RobertStefanescu@users.noreply.github.com>
2020-03-26 21:19:09 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,37 +1,46 @@
 #include <iostream>
 #include <complex>
 #include <algorithm>
+
 using namespace std;
 
-class vector_complex{
+class vector_complex {
 private:
     complex<double> *p;
     int nr_elem;
-    bool static cmp(complex<double>nr1, complex<double>nr2){
-        return(abs(nr1)<abs(nr2));
+
+    bool static cmp(complex<double> nr1, complex<double> nr2) {
+        return (abs(nr1) < abs(nr2));
     }
+
 public:
-    vector_complex (complex<double> nr = 0, int nr_comp = 0 ); ///constructor initializare
-    vector_complex (const vector_complex &v);
-    ~vector_complex ();
-    friend istream& operator >> (istream & input, vector_complex&v);
-    friend ostream& operator << (ostream & output, vector_complex&v);
+    vector_complex(complex<double> nr = 0, int nr_comp = 0); ///constructor initializare
+    vector_complex(const vector_complex &v);
+
+    ~vector_complex();
+
+    friend istream &operator>>(istream &input, vector_complex &v);
+
+    friend ostream &operator<<(ostream &output, vector_complex &v);
     void afisare_module();
+
     void sortare();
+
     complex<double> suma();
-    complex<double> operator *(const vector_complex &v);
+
+    complex<double> operator*(const vector_complex &v);
 
 };
 
-istream& operator >> (istream & input, vector_complex&v){
+istream &operator>>(istream &input, vector_complex &v) {
     double real, img;
-    cout << "introduceti nr elemente: ";
+    cout << "Introduceti numarul de elemente: ";
     input >> v.nr_elem;
     delete[] v.p;
-    complex<double>c;
-    v.p=new complex<double>[v.nr_elem];
-    cout << "format: \"real complex\"\n";
-    for (int i=0;i<v.nr_elem;i++) {
+    complex<double> c;
+    v.p = new complex<double>[v.nr_elem];
+    cout << "format: \"real imaginar\"\n";
+    for (int i = 0; i < v.nr_elem; i++) {
         input >> real >> img;
         c.real(real);
         c.imag(img);
@@ -40,62 +49,70 @@ istream& operator >> (istream & input, vector_complex&v){
     return input;
 }
 
-ostream& operator << (ostream & output, vector_complex&v){
-    output << v.nr_elem << "\n";
-    for (int i=0;i<v.nr_elem;i++)
-        output << v.p[i].real() << " "<< v.p[i].imag()<<" ; ";
-    output << '\n';
+ostream &operator<<(ostream &output, vector_complex &v) {
+    output << "<Vector_complex cu " << v.nr_elem << " elemente: [" ;
+    for (int i = 0; i < v.nr_elem; i++) {
+        if ((int)v.p[i].imag()>0)
+            output <<" " <<v.p[i].real() << " + " << v.p[i].imag() << "i,";
+        else
+            output <<" " << v.p[i].real() << " - " << (-1)*v.p[i].imag() << "i,";
+    }
+    output << "]>" <<'\n';
     return output;
 }
 
-vector_complex::vector_complex(complex<double> nr, int nr_comp ) {//constr init
-    nr_elem=nr_comp;
-    p=new complex<double>[nr_comp];
+vector_complex::vector_complex(complex<double> nr, int nr_comp) {//constr init
+    nr_elem = nr_comp;
+    p = new complex<double>[nr_comp];
 
-    for (int i=0;i<nr_comp;i++)
-        p[i]=nr;
+    for (int i = 0; i < nr_comp; i++)
+        p[i] = nr;
 }
 
-vector_complex::vector_complex (const vector_complex &v) {//constr copiere
-    nr_elem=v.nr_elem;
-    p=new complex<double>[nr_elem];
-    for (int i=0;i<nr_elem;i++)
-        p[i]=v.p[i];
+vector_complex::vector_complex(const vector_complex &v) {//constr copiere
+    nr_elem = v.nr_elem;
+    p = new complex<double>[nr_elem];
+    for (int i = 0; i < nr_elem; i++)
+        p[i] = v.p[i];
 }
 
-vector_complex::~vector_complex (){//desctructor
-    nr_elem=0;
+vector_complex::~vector_complex() {//desctructor
+    nr_elem = 0;
     delete[] p;
 }
 
-void vector_complex::afisare_module(){
-    for (int i=0;i<nr_elem;i++)
-        cout << abs(p[i]) <<" ";
+void vector_complex::afisare_module() {
+    cout << "Vectorul de module sortat:[ ";
+    for (int i = 0; i < nr_elem; i++)
+        cout << abs(p[i]) << ", ";
+    cout << "]";
     cout << '\n';
 }
 
-void vector_complex::sortare(){
-    sort (p,p+nr_elem,cmp);
+void vector_complex::sortare() {
+    sort(p, p + nr_elem, cmp);
 }
 
-complex<double> vector_complex::suma(){
-    complex<double> s=0;
-    for (int i=0;i<nr_elem;i++)
-        s=s+p[i];
+complex<double> vector_complex::suma() {
+    complex<double> s = 0;
+    for (int i = 0; i < nr_elem; i++)
+        s+=p[i];
     return s;
 }
 
-complex<double> vector_complex::operator*(const vector_complex &v){
-    complex<double> produs=0;
-    if (nr_elem!=v.nr_elem)
+complex<double> vector_complex::operator*(const vector_complex &v) {
+    complex<double> produs = 0;
+    if (nr_elem != v.nr_elem) {
+        cout << "Dimensiunile difera!";
         return -1;
-    for (int i=0;i<nr_elem;i++)
-        produs=produs+p[i]*v.p[i];
+    }
+    for (int i = 0; i < nr_elem; i++)
+        produs = produs + p[i] * v.p[i];
     return produs;
 }
 
 int main() {
-    complex <double> nr(2,2);
+    complex<double> nr(2, 2);
     vector_complex v;
     cin >> v;
     vector_complex v2(nr, 5);
@@ -105,6 +122,6 @@ int main() {
     v.afisare_module();
     v.sortare();
     cout << v;
-    cout << v*v3;
+    cout << v * v3;
     return 0;
 }
